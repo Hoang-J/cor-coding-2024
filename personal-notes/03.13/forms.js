39ce53529b2d45ca280758b2e1b366e2f34a291b
COR Coding <corcoding@Coding-16.local>
2024-03-13 20:18:21 -0400

    added login page project

---
+++ personal-notes/03.13/forms.js
@@ -0,0 +1,7 @@
+
+function getEmail() {
+    let email = document.getElementById('email');       //store this info into a variable
+    let password = document.getElementById('password');
+    console.log("Email: ", email.value);
+    console.log("Password: ", password.value);
+}
